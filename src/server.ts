57ac0d3ffd7a0fcbb6bb0e3d9525ae26985de51b
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-08-13 09:47:00 +0700

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
+import routes from "./route";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+app.use("/api", routes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
