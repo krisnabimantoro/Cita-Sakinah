71904686e49f82797255f27842593a9bebadaa87
Krisna Bimantoro <krisnabmntr@gmail.com>
2024-08-31 12:50:15 +0700

    adding validation and filtering route

--- src/controllers/informasi.controller.ts
+++ src/controllers/informasi.controller.ts
@@ -1,90 +1,183 @@
 import { Request, Response } from "express";
 import connect from "../utils/database";
 import informasiModel from "../models/informasi.model";
-import imageInformasi from "../models/imageInformasi.model";
 import removeFile from "../utils/remove.file";
 import { ResultSetHeader } from "mysql2";
+import * as Yup from "yup";
+
+const updateValidationSchema = Yup.object().shape({
+  judul: Yup.string().typeError("Inputan untuk 'judul' harus berupa huruf"),
+  tanggal: Yup.date().typeError("Inputan untuk 'tanggal' harus berupa tanggal yang valid"),
+  deskripsi: Yup.string().typeError("Inputan untuk 'deskripsi' harus berupa huruf"),
+  sekolahId: Yup.number().typeError("Inputan untuk 'sekolahId' harus berupa angka"),
+});
+
+const createValidationSchema = Yup.object().shape({
+  judul: Yup.string().required("Judul harus diisi").typeError("Inputan untuk 'judul' harus berupa huruf"),
+  tanggal: Yup.date().required("Tanggal harus diisi").typeError("Inputan untuk 'tanggal' harus berupa tanggal yang valid"),
+  deskripsi: Yup.string().required("Deskripsi harus diisi").typeError("Inputan untuk 'deskripsi' harus berupa huruf"),
+  sekolahId: Yup.number().required("Sekolah ID harus diisi").typeError("Inputan untuk 'sekolahId' harus berupa angka"),
+});
 
 export default {
   async createData(req: Request, res: Response) {
-    const conn = await connect();
-    const data: informasiModel = req.body;
+    try {
+      await createValidationSchema.validate(req.body);
+      const conn = await connect();
+      const data: informasiModel = req.body;
 
-    const files = req.files as Express.Multer.File[] | undefined;
-    const imagePaths = files ? files.map((file) => file.path.replace(/\\/g, "/")) : [];
-    console.log(imagePaths);
+      const files = req.files as Express.Multer.File[] | undefined;
+      const imagePaths = files ? files.map((file) => file.path.replace(/\\/g, "/")) : [];
+      //   console.log(imagePaths);
+      if (imagePaths.length < 1) return res.status(500).json({ message: "Input gambar kosong" });
 
-    const [insertData] = await conn.query<ResultSetHeader>(`INSERT INTO informasi (judul,tanggal,deskripsi,sekolahId) values (?,?,?,?)`, [
-      data.judul,
-      data.tanggal,
-      data.deskripsi,
-      data.sekolahId,
-    ]);
+      const [insertData] = await conn.query<ResultSetHeader>(`INSERT INTO informasi (judul,tanggal,deskripsi,sekolahId) values (?,?,?,?)`, [
+        data.judul,
+        data.tanggal,
+        data.deskripsi,
+        data.sekolahId,
+      ]);
 
-    const informasiId = insertData.insertId;
+      const informasiId = insertData.insertId;
 
-    for (const imagePath of imagePaths) {
-      await conn.query("INSERT INTO imageInformasi (informasiId, fileName) VALUES (?, ?)", [informasiId, imagePath]);
+      for (const imagePath of imagePaths) {
+        await conn.query("INSERT INTO imageInformasi (informasiId, fileName) VALUES (?, ?)", [informasiId, imagePath]);
+      }
+      return res.status(201).json({
+        message: "Informasi berhasil dibuat!",
+        insertData,
+        imagePaths,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal membuat informasi!",
+      });
     }
-    return res.status(201).json({
-      message: "Kegiatan berhasil dibuat!",
-      insertData,
-      imagePaths,
-    });
   },
   async updateData(req: Request, res: Response) {
-    const informasiId = req.params.id;
-    const idImage = req.query.idImage;
-    const data: informasiModel = req.body;
-    // const imgKegiatan:
-    const conn = await connect();
+    try {
+      await updateValidationSchema.validate(req.body);
+      const informasiId = req.params.id;
+      const idImage = req.query.idImage;
+      const data: informasiModel = req.body;
+      // const imgKegiatan:
+      const conn = await connect();
 
-    if (idImage) {
-      const [oldImage] = await conn.query<any>(`select fileName from imageInformasi where idImage = ? and informasiId = ?`, [
-        idImage,
-        informasiId,
-      ]);
-      removeFile(oldImage[0].fileName);
+      if (idImage) {
+        const [oldImage] = await conn.query<any>(`select fileName from imageInformasi where idImage = ? and informasiId = ?`, [
+          idImage,
+          informasiId,
+        ]);
+        removeFile(oldImage[0].fileName);
+      }
+      const imagePaths = req.file as Express.Multer.File | undefined;
+      const imageUrl = imagePaths?.path.replace(/\\/g, "/");
+      //   console.log(imageUrl);
+
+      let updateImageInformasi;
+      if (imagePaths?.filename) {
+        console.log("cek");
+        updateImageInformasi = await conn.query(`UPDATE  imageInformasi set  fileName =? WHERE informasiId  = ? and idImage=?`, [
+          imageUrl,
+          informasiId,
+          idImage,
+        ]);
+      }
+
+      const updateInformasi = await conn.query(`UPDATE  informasi set ? WHERE id = ?`, [data, informasiId]);
+      // await removeFile()
+
+      return res.status(200).json({
+        message: "Kegiatan berhasil diupdated!",
+        updateKegiatan: updateInformasi,
+        updateImageKegiatan: updateImageInformasi,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal update data informasi!",
+      });
     }
-    const imagePaths = req.file as Express.Multer.File | undefined;
-    const imageUrl = imagePaths?.path.replace(/\\/g, "/");
-    console.log(imageUrl);
-
-    let updateImageInformasi;
-    if (imagePaths?.filename) {
-      console.log("cek");
-      updateImageInformasi = await conn.query(`UPDATE  imageInformasi set  fileName =? WHERE informasiId  = ? and idImage=?`, [
-        imageUrl,
-        informasiId,
-        idImage,
-      ]);
-    }
-
-    const updateKegiatan = await conn.query(`UPDATE  informasi set ? WHERE id = ?`, [data, informasiId]);
-    // await removeFile()
-
-    return res.json({
-      message: "Kegiatan berhasil diupdated!",
-      updateKegiatan,
-      updateImageKegiatan: updateImageInformasi,
-    });
   },
   async deleteData(req: Request, res: Response) {
-    const conn = await connect();
-    const id = req.params.id;
+    try {
+      const conn = await connect();
+      const id = req.params.id;
 
-    const [imagePath] = await conn.query<any>(`select fileName from imageInformasi where informasiId = ?`, [id]);
+      const [imagePath] = await conn.query<any>(`select fileName from imageInformasi where informasiId = ?`, [id]);
 
-    for (const imageDelete of imagePath) {
-      removeFile(imageDelete.fileName);
+      for (const imageDelete of imagePath) {
+        removeFile(imageDelete.fileName);
+      }
+
+      const deleteImage = await conn.query(`DELETE FROM imageInformasi  WHERE informasiId = ?`, [id]);
+      const result = await conn.query(`DELETE FROM informasi  WHERE id = ?`, [id]);
+      return res.status(200).json({
+        message: "Kegiatan berhasil dihapus!",
+        result,
+        deleteImage,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal hapus data informasi!",
+      });
     }
+  },
+  async displayData(req: Request, res: Response) {
+    try {
+      const conn = await connect();
+      const [rows] = await conn.query(
+        `select i.*, group_concat(ii.fileName) as fileName, s.namaSekolah from informasi i left join imageInformasi ii on i.id = ii.informasiId left join sekolah s on i.sekolahId = s.id group by i.id`
+      );
+      const result = (rows as any[]).map((row) => ({
+        id: row.id,
+        judul: row.judul,
+        tanggal: row.tanggal,
+        deskripsi: row.deskripsi,
+        fileName: row.fileName ? row.fileName.split(",") : [],
+        namaSekolah: row.namaSekolah,
+      }));
+      return res.json({ result });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal menampilkan data informasi!",
+      });
+    }
+  },
+  async filterData(req: Request, res: Response) {
+    try {
+      const sekolahId = req.query.sekolahId;
+      const conn = await connect();
 
-    const deleteImage = await conn.query(`DELETE FROM imageInformasi  WHERE informasiId = ?`, [id]);
-    const result = await conn.query(`DELETE FROM informasi  WHERE id = ?`, [id]);
-    return res.json({
-      message: "Kegiatan berhasil dihapus!",
-      result,
-      deleteImage,
-    });
+      console.log("cek");
+      if (sekolahId) {
+        const [rows] = await conn.query(
+          `select i.*, group_concat(ii.fileName) as fileName, s.namaSekolah from informasi i left join imageInformasi ii on i.id = ii.informasiId left join sekolah s on i.sekolahId = s.id where i.sekolahId = ? group by i.id `,
+          [sekolahId]
+        );
+        const result = (rows as any[]).map((row) => ({
+          id: row.id,
+          judul: row.judul,
+          tanggal: row.tanggal,
+          deskripsi: row.deskripsi,
+          fileName: row.fileName ? row.fileName.split(",") : [],
+          namaSekolah: row.namaSekolah,
+        }));
+        return res.json(result);
+      }
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal menampilkan data informasi!",
+      });
+    }
   },
 };
--- src/controllers/kegiatan.controller.ts
+++ src/controllers/kegiatan.controller.ts
@@ -3,122 +3,235 @@ import connect from "../utils/database";
 import kegiatan from "../models/kegiatan.model";
 import { ResultSetHeader } from "mysql2";
 import removeFile from "../utils/remove.file";
+import * as Yup from "yup";
+
+const updateValidationSchema = Yup.object().shape({
+  judul: Yup.string().typeError("Inputan untuk 'judul' harus berupa huruf"),
+  tanggal: Yup.date().typeError("Inputan untuk 'tanggal' harus berupa tanggal yang valid"),
+  deskripsi: Yup.string().typeError("Inputan untuk 'deskripsi' harus berupa huruf"),
+  jenisKegiatan: Yup.number().typeError("Inputan untuk 'jenisKegiatan' harus berupa angka"),
+  sekolahId: Yup.number().typeError("Inputan untuk 'sekolahId' harus berupa angka"),
+});
+
+const createValidationSchema = Yup.object().shape({
+  judul: Yup.string().required("Judul wajib diisi").typeError("Inputan untuk 'judul' harus berupa huruf"),
+  tanggal: Yup.date().required("Tanggal wajib diisi").typeError("Inputan untuk 'tanggal' harus berupa tanggal yang valid"),
+  deskripsi: Yup.string().required("Deskripsi wajib diisi").typeError("Inputan untuk 'deskripsi' harus berupa huruf"),
+  jenisKegiatan: Yup.number().required("Jenis kegiatan wajib diisi").typeError("Inputan untuk 'jenisKegiatan' harus berupa angka"),
+  sekolahId: Yup.number().required("Sekolah ID wajib diisi").typeError("Inputan untuk 'sekolahId' harus berupa angka"),
+});
 
 export default {
   async displayData(req: Request, res: Response) {
-    const conn = await connect();
-    const [rows] = await conn.query(
-      `SELECT k.id AS kegiatanId, k.judul, k.tanggal, k.deskripsi, kk.namaKegiatan, GROUP_CONCAT(ik.fileName) AS fileName, s.namaSekolah FROM kegiatan k LEFT JOIN kategorikegiatan kk ON k.jenisKegiatan = kk.id LEFT JOIN imageKegiatan ik ON k.id = ik.kegiatanId left JOIN sekolah s ON k.sekolahId = s.id GROUP BY k.id `
-    );
+    try {
+      const conn = await connect();
+      const [rows] = await conn.query(
+        `
+        select 
+          k.*, group_concat(ik.fileName) as fileName, kk.namaKegiatan, s.namaSekolah FROM kegiatan k 
+        LEFT JOIN
+          kategorikegiatan kk ON k.jenisKegiatan = kk.id
+        LEFT JOIN 
+          imageKegiatan ik ON k.id = ik.kegiatanId 
+        left JOIN
+          sekolah s ON k.sekolahId = s.id 
+        GROUP BY 
+          k.id 
+        `
+      );
 
-    const result = (rows as any[]).map((row) => ({
-      kegiatanId: row.kegiatanId,
-      judul: row.judul,
-      tanggal: row.tanggal,
-      deskripsi: row.deskripsi,
-      namaKegiatan: row.namaKegiatan,
-      fileNames: row.fileNames ? row.fileNames.split(",") : [],
-    }));
-    return res.json(result);
+      const result = (rows as any[]).map((row) => ({
+        id: row.id,
+        judul: row.judul,
+        tanggal: row.tanggal,
+        deskripsi: row.deskripsi,
+        namaKegiatan: row.namaKegiatan,
+        namaSekolah: row.namaSekolah,
+        fileName: row.fileName ? row.fileName.split(",") : [],
+      }));
+
+      console.log(result[0].fileName);
+      return res.json(result);
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal menampilkan data kegiatan!",
+      });
+    }
   },
   async createData(req: Request, res: Response) {
-    const conn = await connect();
-    const data: kegiatan = req.body;
+    try {
+      await createValidationSchema.validate(req.body);
+      const conn = await connect();
+      const data: kegiatan = req.body;
 
-    const files = req.files as Express.Multer.File[] | undefined;
-    const imagePaths = files ? files.map((file) => file.path.replace(/\\/g, "/")) : [];
-    // const imageUrl =
+      const files = req.files as Express.Multer.File[] | undefined;
+      const imagePaths = files ? files.map((file) => file.path.replace(/\\/g, "/")) : [];
+      // const imageUrl =
+      if (imagePaths.length < 1) return res.status(500).json({ message: "Input gambar kosong" });
 
-    const [insertData] = await conn.query<ResultSetHeader>(`INSERT INTO kegiatan set ?`, [data]);
+      const [insertData] = await conn.query<ResultSetHeader>(`INSERT INTO kegiatan set ?`, [data]);
 
-    const kegiatanId = insertData.insertId;
+      const kegiatanId = insertData.insertId;
 
-    for (const imagePath of imagePaths) {
-      await conn.query("INSERT INTO imageKegiatan (kegiatanId, fileName) VALUES (?, ?)", [kegiatanId, imagePath]);
+      for (const imagePath of imagePaths) {
+        await conn.query("INSERT INTO imageKegiatan (kegiatanId, fileName) VALUES (?, ?)", [kegiatanId, imagePath]);
+      }
+      return res.status(201).json({
+        message: "Kegiatan berhasil dibuat!",
+        insertData,
+        imagePaths,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal membuat kegiatan!",
+      });
     }
-    return res.status(201).json({
-      message: "Kegiatan berhasil dibuat!",
-      insertData,
-      imagePaths,
-    });
   },
   async updateData(req: Request, res: Response) {
-    const kegiatanId = req.params.id;
-    const idImage = req.query.idImage;
-    const data: kegiatan = req.body;
-    // const imgKegiatan:
-    const conn = await connect();
+    try {
+      await updateValidationSchema.validate(req.body);
+      const kegiatanId = req.params.id;
+      const idImage = req.query.idImage;
+      const data: kegiatan = req.body;
+      // const imgKegiatan:
+      const conn = await connect();
 
-    if (idImage) {
-      const [oldImage] = await conn.query<any>(`select fileName from imageKegiatan where idImage = ? and kegiatanId = ?`, [
-        idImage,
-        kegiatanId,
-      ]);
-      removeFile(oldImage[0].fileName);
+      if (idImage) {
+        const [oldImage] = await conn.query<any>(`select fileName from imageKegiatan where idImage = ? and kegiatanId = ?`, [
+          idImage,
+          kegiatanId,
+        ]);
+        removeFile(oldImage[0].fileName);
+      }
+      const imagePaths = req.file as Express.Multer.File | undefined;
+      const imageUrl = imagePaths?.path.replace(/\\/g, "/");
+
+      let updateImageKegiatan;
+      if (imagePaths?.filename) {
+        // console.log("cek");
+        updateImageKegiatan = await conn.query(`UPDATE  imageKegiatan set  fileName =? WHERE kegiatanId  = ? and idImage=?`, [
+          imageUrl,
+          kegiatanId,
+          idImage,
+        ]);
+      }
+
+      const updateKegiatan = await conn.query(`UPDATE  kegiatan set ? WHERE id = ?`, [data, kegiatanId]);
+      // await removeFile()
+
+      return res.status(200).json({
+        message: "Kegiatan berhasil diupdated!",
+        updateKegiatan,
+        updateImageKegiatan,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal update data kegiatan!",
+      });
     }
-    const imagePaths = req.file as Express.Multer.File | undefined;
-    const imageUrl = imagePaths?.path.replace(/\\/g, "/");
-
-    let updateImageKegiatan;
-    if (imagePaths?.filename) {
-      // console.log("cek");
-      updateImageKegiatan = await conn.query(`UPDATE  imageKegiatan set  fileName =? WHERE kegiatanId  = ? and idImage=?`, [
-        imageUrl,
-        kegiatanId,
-        idImage,
-      ]);
-    }
-
-    const updateKegiatan = await conn.query(`UPDATE  kegiatan set ? WHERE id = ?`, [data, kegiatanId]);
-    // await removeFile()
-
-    return res.json({
-      message: "Kegiatan berhasil diupdated!",
-      updateKegiatan,
-      updateImageKegiatan,
-    });
   },
   async deleteData(req: Request, res: Response) {
-    const id = req.params.id;
-    const conn = await connect();
+    try {
+      const id = req.params.id;
+      const conn = await connect();
 
-    const [imagePath] = await conn.query<any>(`select fileName from imageKegiatan where kegiatanId = ?`, [id]);
+      const [imagePath] = await conn.query<any>(`select fileName from imageKegiatan where kegiatanId = ?`, [id]);
 
-    for (const imageDelete of imagePath) {
-      removeFile(imageDelete.fileName);
+      for (const imageDelete of imagePath) {
+        removeFile(imageDelete.fileName);
+      }
+
+      const deleteImage = await conn.query(`DELETE FROM imageKegiatan  WHERE kegiatanId = ?`, [id]);
+      const result = await conn.query(`DELETE FROM kegiatan  WHERE id = ?`, [id]);
+      return res.json({
+        message: "Kegiatan berhasil dihapus!",
+        result,
+        deleteImage,
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        information: err.message,
+        message: "Gagal menghapus kegiatan!",
+      });
     }
-
-    const deleteImage = await conn.query(`DELETE FROM imageKegiatan  WHERE kegiatanId = ?`, [id]);
-    const result = await conn.query(`DELETE FROM kegiatan  WHERE id = ?`, [id]);
-    return res.json({
-      message: "Kegiatan berhasil dihapus!",
-      result,
-      deleteImage,
-    });
   },
-  async filterSekolah(req: Request, res: Response) {
+  async filterDisplay(req: Request, res: Response) {
     const sekolahId = req.query.sekolahId;
     const jenisKegiatan = req.query.jenisKegiatan;
     const conn = await connect();
 
+    function displayResult(rows: any) {
+      return (rows as any[]).map((row) => ({
+        id: row.id,
+        judul: row.judul,
+        tanggal: row.tanggal,
+        deskripsi: row.deskripsi,
+        namaKegiatan: row.namaKegiatan,
+        namaSekolah: row.namaSekolah,
+        fileName: row.fileName ? row.fileName.split(",") : [],
+      }));
+    }
+
     if (jenisKegiatan && sekolahId) {
+      console.log("cek");
       const result = await conn.query(
-        `SELECT k.*, kk.namaKegiatan, s.namaSekolah  FROM kegiatan k JOIN kategoriKegiatan kk ON k.jenisKegiatan = kk.id JOIN sekolah s ON k.sekolahId = s.id where sekolahId=? && jenisKegiatan=?`,
+        `    
+        select 
+          k.*, group_concat(ik.fileName) as fileName, kk.namaKegiatan, s.namaSekolah FROM kegiatan k 
+        LEFT JOIN
+          kategorikegiatan kk ON k.jenisKegiatan = kk.id
+        LEFT JOIN 
+          imageKegiatan ik ON k.id = ik.kegiatanId 
+        left JOIN
+          sekolah s ON k.sekolahId = s.id 
+        where 
+          sekolahId=? && jenisKegiatan=?
+        GROUP BY 
+          k.id  `,
         [sekolahId, jenisKegiatan]
       );
-      return res.json(result[0]);
+      return res.json(displayResult(result[0]));
     } else if (!sekolahId) {
       const result = await conn.query(
-        `SELECT k.*, kk.namaKegiatan , s.namaSekolah  FROM kegiatan k JOIN kategoriKegiatan kk ON k.jenisKegiatan = kk.id JOIN sekolah s ON k.sekolahId = s.id where jenisKegiatan=?`,
+        `select 
+          k.*, group_concat(ik.fileName) as fileName, kk.namaKegiatan, s.namaSekolah FROM kegiatan k 
+        LEFT JOIN
+          kategorikegiatan kk ON k.jenisKegiatan = kk.id
+        LEFT JOIN 
+          imageKegiatan ik ON k.id = ik.kegiatanId 
+        left JOIN
+          sekolah s ON k.sekolahId = s.id 
+        where 
+          jenisKegiatan=?
+        GROUP BY 
+          k.id `,
         [jenisKegiatan]
       );
-      return res.json(result[0]);
+      return res.json(displayResult(result[0]));
     } else {
       const result = await conn.query(
-        `SELECT k.*, kk.namaKegiatan, s.namaSekolah FROM kegiatan k JOIN kategoriKegiatan kk ON k.jenisKegiatan = kk.id JOIN sekolah s ON k.sekolahId = s.id where sekolahId=? `,
+        `select 
+          k.*, group_concat(ik.fileName) as fileName, kk.namaKegiatan, s.namaSekolah FROM kegiatan k 
+        LEFT JOIN
+          kategorikegiatan kk ON k.jenisKegiatan = kk.id
+        LEFT JOIN 
+          imageKegiatan ik ON k.id = ik.kegiatanId 
+        left JOIN
+          sekolah s ON k.sekolahId = s.id 
+        where 
+          sekolahId=?
+        GROUP BY 
+          k.id `,
         [sekolahId]
       );
-      return res.json(result[0]);
+      return res.json(displayResult(result[0]));
     }
   },
 };
