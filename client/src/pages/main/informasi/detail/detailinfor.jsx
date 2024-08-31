75fd22466d6a4cd289004ca7fcc9fb124d37a1da
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2024-08-31 10:02:42 +0700

    update: button kembali at detail kegiatan and informasi

--- client/src/pages/main/informasi/detail/detailinfor.jsx
+++ client/src/pages/main/informasi/detail/detailinfor.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { dataInfor } from "../../../../data/datainfor";
 import NotFoundPage from "../../../notfound";
 import { FaRegBuilding } from "react-icons/fa";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import { Download, Zoom } from "yet-another-react-lightbox/plugins";
+import { RiArrowLeftLine } from "react-icons/ri";
 
 const DetailInforPage = () => {
   const { id } = useParams();
   const informasi = dataInfor.find((infor) => infor.id === parseInt(id));
   const [isOpen, setIsOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = `Cita Sakinah | Detail Informasi #${id}`;
@@ -29,49 +31,58 @@ const DetailInforPage = () => {
   return (
     <>
       <section className="mx-[50px] md:mx-[120px] mb-[150px]">
-        <div className="mt-[135px] flex flex-col md:flex-row gap-10">
-          <div className="md:w-[60%] flex flex-col gap-10">
-            <img
-              src={informasi.img[0]}
-              alt={`img-detail-0`}
-              className="rounded-2xl w-full cursor-pointer"
-              onClick={() => handleOpenLightbox(0)}
-            />
-            <div className="flex gap-5 flex-col">
-              <div className="flex mb-2 text-white font-semibold sm:text-lg gap-2">
-                {informasi.tagSekolah.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="bg-button px-2 py-1 rounded-md flex items-center gap-2 capitalize"
-                  >
-                    <FaRegBuilding /> {tag}
-                  </span>
-                ))}
-              </div>
-              <div>
-                <h1 className="font-bold text-main text-2xl sm:text-4xl">
-                  {informasi.title}
-                </h1>
-                <h3 className="text-abugelap font-medium text-lg">
-                  {informasi.date}
-                </h3>
-              </div>
-              <p className="text-abugelap text-base text-justify">
-                {informasi.detail}
-              </p>
-            </div>
-          </div>
-
-          <div className="md:w-[40%] grid grid-cols-2 gap-5 h-fit">
-            {informasi.img.slice(1).map((src, index) => (
+        <div className="mt-[135px] flex flex-col gap-5">
+          <button
+            className="flex items-center gap-3 font-medium text-xl cursor-pointer w-fit hover:scale-105 duration-200 text-main"
+            onClick={() => navigate(-1)}
+          >
+            <RiArrowLeftLine size={24} />
+            <span>Kembali</span>
+          </button>
+          <div className="flex flex-col md:flex-row gap-10">
+            <div className="md:w-[60%] flex flex-col gap-10">
               <img
-                key={index + 1}
-                src={src}
-                alt={`img-detail-${index + 1}`}
+                src={informasi.img[0]}
+                alt={`img-detail-0`}
                 className="rounded-2xl w-full cursor-pointer"
-                onClick={() => handleOpenLightbox(index + 1)}
+                onClick={() => handleOpenLightbox(0)}
               />
-            ))}
+              <div className="flex gap-5 flex-col">
+                <div className="flex mb-2 text-white font-semibold sm:text-lg gap-2">
+                  {informasi.tagSekolah.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="bg-button px-2 py-1 rounded-md flex items-center gap-2 capitalize"
+                    >
+                      <FaRegBuilding /> {tag}
+                    </span>
+                  ))}
+                </div>
+                <div>
+                  <h1 className="font-bold text-main text-2xl sm:text-4xl">
+                    {informasi.title}
+                  </h1>
+                  <h3 className="text-abugelap font-medium text-lg">
+                    {informasi.date}
+                  </h3>
+                </div>
+                <p className="text-abugelap text-base text-justify">
+                  {informasi.detail}
+                </p>
+              </div>
+            </div>
+
+            <div className="md:w-[40%] grid grid-cols-2 gap-5 h-fit">
+              {informasi.img.slice(1).map((src, index) => (
+                <img
+                  key={index + 1}
+                  src={src}
+                  alt={`img-detail-${index + 1}`}
+                  className="rounded-2xl w-full cursor-pointer"
+                  onClick={() => handleOpenLightbox(index + 1)}
+                />
+              ))}
+            </div>
           </div>
         </div>
         {isOpen && (
--- client/src/pages/main/kegiatan/detail/detailkeg.jsx
+++ client/src/pages/main/kegiatan/detail/detailkeg.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { dataKeg } from "../../../../data/datakeg";
 import NotFoundPage from "../../../notfound";
 import { LuClipboardCheck } from "react-icons/lu";
@@ -7,12 +7,14 @@ import { FaRegBuilding } from "react-icons/fa";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import { Download, Zoom } from "yet-another-react-lightbox/plugins";
+import { RiArrowLeftLine } from "react-icons/ri";
 
 const DetailKegPage = () => {
   const { id } = useParams();
   const kegiatan = dataKeg.find((keg) => keg.id === parseInt(id));
   const [isOpen, setIsOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = `Cita Sakinah | Detail Kegiatan #${id}`;
@@ -30,48 +32,57 @@ const DetailKegPage = () => {
   return (
     <>
       <section className="mx-[50px] md:mx-[120px] mb-[150px]">
-        <div className="mt-[135px] flex flex-col md:flex-row gap-10">
-          <div className="md:w-[60%] flex flex-col gap-10">
-            <img
-              src={kegiatan.img[0]}
-              alt={`img-detail-0`}
-              className="rounded-2xl w-full cursor-pointer"
-              onClick={() => handleOpenLightbox(0)}
-            />
-            <div className="flex gap-5 flex-col">
-              <div className="flex mb-2 text-white font-semibold sm:text-lg gap-4">
-                <span className="bg-button px-2 py-1 rounded-md flex gap-2 items-center capitalize">
-                  <LuClipboardCheck />
-                  {kegiatan.tagUtama}
-                </span>
-                <span className="bg-button px-2 py-1 rounded-md flex items-center gap-2 capitalize">
-                  <FaRegBuilding /> {kegiatan.tagSekolah}
-                </span>
-              </div>
-              <div>
-                <h1 className="font-bold text-main text-2xl sm:text-4xl">
-                  {kegiatan.titleCard}
-                </h1>
-                <h3 className="text-abugelap font-medium text-lg">
-                  {kegiatan.tanggal}
-                </h3>
-              </div>
-              <p className="text-abugelap text-base text-justify">
-                {kegiatan.detail}
-              </p>
-            </div>
-          </div>
-
-          <div className="md:w-[40%] grid grid-cols-2 gap-5 h-fit">
-            {kegiatan.img.slice(1).map((src, index) => (
+        <div className="mt-[135px] flex flex-col gap-5">
+          <button
+            className="flex items-center gap-3 font-medium text-xl cursor-pointer w-fit hover:scale-105 duration-200 text-main"
+            onClick={() => navigate(-1)}
+          >
+            <RiArrowLeftLine size={24} />
+            <span>Kembali</span>
+          </button>
+          <div className="flex flex-col md:flex-row gap-10">
+            <div className="md:w-[60%] flex flex-col gap-10">
               <img
-                key={index + 1}
-                src={src}
-                alt={`img-detail-${index + 1}`}
+                src={kegiatan.img[0]}
+                alt={`img-detail-0`}
                 className="rounded-2xl w-full cursor-pointer"
-                onClick={() => handleOpenLightbox(index + 1)}
+                onClick={() => handleOpenLightbox(0)}
               />
-            ))}
+              <div className="flex gap-5 flex-col">
+                <div className="flex mb-2 text-white font-semibold sm:text-lg gap-4">
+                  <span className="bg-button px-2 py-1 rounded-md flex gap-2 items-center capitalize">
+                    <LuClipboardCheck />
+                    {kegiatan.tagUtama}
+                  </span>
+                  <span className="bg-button px-2 py-1 rounded-md flex items-center gap-2 capitalize">
+                    <FaRegBuilding /> {kegiatan.tagSekolah}
+                  </span>
+                </div>
+                <div>
+                  <h1 className="font-bold text-main text-2xl sm:text-4xl">
+                    {kegiatan.titleCard}
+                  </h1>
+                  <h3 className="text-abugelap font-medium text-lg">
+                    {kegiatan.tanggal}
+                  </h3>
+                </div>
+                <p className="text-abugelap text-base text-justify">
+                  {kegiatan.detail}
+                </p>
+              </div>
+            </div>
+
+            <div className="md:w-[40%] grid grid-cols-2 gap-5 h-fit">
+              {kegiatan.img.slice(1).map((src, index) => (
+                <img
+                  key={index + 1}
+                  src={src}
+                  alt={`img-detail-${index + 1}`}
+                  className="rounded-2xl w-full cursor-pointer"
+                  onClick={() => handleOpenLightbox(index + 1)}
+                />
+              ))}
+            </div>
           </div>
         </div>
         {isOpen && (
